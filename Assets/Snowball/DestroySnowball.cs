06caa8a36de9107230b0bc953d890ad0cee31d14
S.Oran <75443930+Orange4337@users.noreply.github.com>
2024-02-07 20:40:53 -0600

    Target Changes and more
    Added Target Folder
    Tagged Snowballs Balls
    Added 3 Targets to scene
    Added Windows above walls
    Editted the Destroy snowball script to ignore tables and player (hopefully)
    Added a (commented) idea for respawn in destroy script

--- Assets/Snowball/DestroySnowball.cs
+++ Assets/Snowball/DestroySnowball.cs
@@ -6,14 +6,19 @@ public class DestorySnowball : MonoBehaviour
 {
     public GameObject splat;
 
+    //Respawn? snowball prefab and point on table like joejeffsnap ballSpawnLocation
+    //public GameObject snowball;
+    //public Transform spawnPoint;
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision != null)
-        {
+        if (!collision.gameObject.CompareTag("Splatproof")){
             
             Destroy(gameObject);
             gameObject.SetActive(false);
             Instantiate(splat, transform.position, transform.rotation);
+            //Respawn?
+            //Instantiate(snowball, new Vector3(spawnPoint.position, Quaternion.identity);
         }
     }
 
